45e899c8236b7c9920bf91a897bd21e725b62902
hehehe84 <antoine.picott@gmail.com>
2023-03-14 16:39:44 +0100

    changes

--- client/src/components/VotingInterface/Balance.jsx
+++ client/src/components/VotingInterface/Balance.jsx
@@ -3,7 +3,7 @@ function Balance({balance}) {
 
     return (
       <div className="bal">
-        Vous avez une balance de {balance} de token
+        Vous avez une balance de {balance} de tokens.
       </div>
       );
   }
--- client/src/components/VotingInterface/index.jsx
+++ client/src/components/VotingInterface/index.jsx
@@ -1,25 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import useEth from "../../contexts/EthContext/useEth";
 import Address from "./Address";
 import Balance from "./Balance";
 import Button from "./Button";
 
 
-function votingInterface() {
+function VotingInterface() {
   const { state: { contract, accounts } } = useEth();
-  const [balance, setBalance] = useState();
+  const [balance, setBalance] = useState("");
 
   const refreshBalance = async () => {
     const value = await contract.methods.balanceOf(accounts[0]).call({ from: accounts[0] });
     setBalance(value);
   }
-
-  useEffect( () => {
-    if (contract?.methods) {
-        refreshBalance();
-    }
-  }, [contract]);
-
 
 
   return (
@@ -32,4 +25,4 @@ function votingInterface() {
   );
 }
 
-export default votingInterface;
+export default VotingInterface;
