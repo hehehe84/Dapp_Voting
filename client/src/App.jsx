2f99b28c88483b1e451878152ede511e9cbca656
hehehe84 <antoine.picott@gmail.com>
2023-03-15 18:41:26 +0100

    add of Chakra-ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,22 @@
 import { EthProvider } from "./contexts/EthContext";
 import Intro from "./components/intro.jsx";
 // import VotingInteface from "./components/VotingInterface/index";
+import { ChakraProvider } from '@chakra-ui/react'
 
 
 function App() {
   return (
-    <EthProvider>
-      <div id="App">
-        <div className="container">
-          <Intro />
-          {/* <hr/>
-          <VotingInterface /> */}
+    <ChakraProvider>
+      <EthProvider>
+        <div id="App">
+          <div className="container">
+            <Intro />
+            {/* <hr/>
+            <VotingInterface /> */}
+          </div>
         </div>
-      </div>
-    </EthProvider>
+      </EthProvider>
+    </ChakraProvider>
   );
 }
 
