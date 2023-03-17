ada924b752f5e13575c8613f323828685699bf29
XaViPanDx <83515045+XaViPanDx@users.noreply.github.com>
2023-03-18 00:02:09 +0100

    Update AddProposal.jsx
    Composant Proposal

--- client/src/components/VotingInterface/Voter/AddProposal.jsx
+++ client/src/components/VotingInterface/Voter/AddProposal.jsx
@@ -1,1 +1,31 @@
 import { Button, ButtonGroup } from '@chakra-ui/react'
+
+import { useState } from 'react';
+import useEth from "../../contexts/EthContext/useEth";
+
+function Proposal() {
+  const { state: { contract, accounts } } = useEth();
+  const [proposal, setProposal] = useState('');
+  
+  const handleInputChange = (e) => {
+    setProposal(e.target.value);
+  };
+
+  const addproposal = async () =>{
+      await contract.methods.addProposal(proposal).call({ from: accounts[0] });
+      await contract.methods.addProposal(proposal).send({ from: accounts[0] });
+  }
+
+  return (
+    <div>
+        <input
+          type="text"
+          placeholder="proposal"
+          value={proposal}
+          onChange={handleInputChange}
+        />
+        <button onClick={addproposal}>Add Proposal</button>  
+    </div>
+  );
+}
+export default Proposal;
