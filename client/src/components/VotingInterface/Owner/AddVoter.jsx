a56eb55e3ef4f0d3ab5f45e7d6b666f17dfcc447
XaViPanDx <83515045+XaViPanDx@users.noreply.github.com>
2023-03-18 01:17:40 +0100

    Update AddVoter.jsx
    Composant add voter à améliorer

--- client/src/components/VotingInterface/Owner/AddVoter.jsx
+++ client/src/components/VotingInterface/Owner/AddVoter.jsx
@@ -1,1 +1,34 @@
 import { Button, ButtonGroup } from '@chakra-ui/react'
+
+import { useState } from 'react';
+import useEth from "../../contexts/EthContext/useEth";
+
+function AddVoter() {
+  const { state: { contract, accounts, web3 } } = useEth();
+  const [voter, setVoter] = useState('');
+  
+  const handleInputChange = (e) => {
+    setVoter(e.target.value);
+  };
+
+  async function addVoter() {
+    if (web3.utils.isAddress(voter)) {
+        await contract.methods.addVoter(voter).call({ from: accounts[0] });
+        await contract.methods.addVoter(voter).send({ from: accounts[0] });
+        setVoter(voter);    
+  };
+}
+  return (
+    <div>
+        <input
+          type="text"
+          placeholder="Voter Address"
+          value={voter}
+          onChange={handleInputChange}
+        />
+        <button onClick={addVoter}>Add Voter</button>  
+    </div>
+  );
+}
+
+export default AddVoter;
