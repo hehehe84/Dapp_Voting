4d801a65aa56642b66783a246d0cb1e51d55ccd4
XaViPanDx <83515045+XaViPanDx@users.noreply.github.com>
2023-03-18 03:08:53 +0100

    Sessions functions
    Ajout des 4 fonctions de Sessions

--- client/src/components/VotingInterface/Owner/EndProposals.jsx
+++ client/src/components/VotingInterface/Owner/EndProposals.jsx
@@ -1,1 +1,21 @@
 import { Button, ButtonGroup } from '@chakra-ui/react'
+
+import useEth from "../../contexts/EthContext/useEth";
+
+function EndProposalRegistering() {
+  const { state: { contract, accounts } } = useEth();
+
+  const proposalRegisteringEnd = async () => {
+    await contract.methods.endProposalsRegistering().send({ from: accounts[0] });
+  };
+
+  return (
+    <div className="endProp">
+      <button onClick={proposalRegisteringEnd}>
+        EndProposalRegistration
+      </button>
+    </div>
+  );
+}
+
+export default EndProposalRegistering;
--- client/src/components/VotingInterface/Owner/StartProposals.jsx
+++ client/src/components/VotingInterface/Owner/StartProposals.jsx
@@ -1,1 +1,21 @@
 import { Button, ButtonGroup } from '@chakra-ui/react'
+
+import useEth from "../../contexts/EthContext/useEth";
+
+function StartProposalRegistering() {
+  const { state: {contract, accounts} } = useEth();
+
+  const proposalRegisteringStart = async () => {
+    await contract.methods.startProposalsRegistering().send({ from: accounts[0] });
+  };
+
+  return (
+    <div className="startProp">
+      <button onClick={proposalRegisteringStart}>
+        StartProposalRegistering
+      </button>
+    </div>
+  );
+}
+
+export default StartProposalRegistering;
