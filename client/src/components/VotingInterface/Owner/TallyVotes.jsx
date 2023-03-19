a8515d245481e0470cd78313f1d561cb6605e597
XaViPanDx <83515045+XaViPanDx@users.noreply.github.com>
2023-03-19 02:58:28 +0100

    Update TallyVotes.jsx
    Tally Votes

--- client/src/components/VotingInterface/Owner/TallyVotes.jsx
+++ client/src/components/VotingInterface/Owner/TallyVotes.jsx
@@ -2,6 +2,7 @@ import { Button, ButtonGroup } from '@chakra-ui/react'
 
 import useEth from "../../contexts/EthContext/useEth";
 
+<<<<<<< Updated upstream
 function TallyVotes(setCurrentStatus) {
   const { state: {contract, accounts} } = useEth();
 
@@ -14,10 +15,27 @@ function TallyVotes(setCurrentStatus) {
     <div className="tallyVote">
       <button onClick={tallyVotes}>
         Tally Votes
+=======
+function TallyVotes() {
+  const { state: { contract, accounts } } = useEth();
+
+  const countVotes = async () => {
+    await contract.methods.tallyVotes().send({ from: accounts[0] });
+  };
+
+  return (
+    <div className="btns">
+      <button onClick={countVotes} className="">
+        Count Votes
+>>>>>>> Stashed changes
       </button>
     </div>
   );
 }
 
+<<<<<<< Updated upstream
 export default StartProposalRegistering;
 
+=======
+export default TallyVotes;
+>>>>>>> Stashed changes
