2e1932b80c7e382fa91d49de95ebac1a14cb96fe
XaViPanDx <83515045+XaViPanDx@users.noreply.github.com>
2023-03-20 15:44:36 +0100

    Add voter component
    Implémentation add voter composant avec bouton

--- client/src/components/VotingInterface/Owner/AddVoter.jsx
+++ client/src/components/VotingInterface/Owner/AddVoter.jsx
@@ -1,42 +1,38 @@
 
-import { useState } from 'react';
+//import { useState } from 'react';
 import useEth from "../../../contexts/EthContext/useEth";
 import { Button } from '@chakra-ui/react';
 
-function AddVoter({currentStatus}) {
+function AddVoter({isOwner, voterAddr, setVoterAddr}) {
   const { state: { contract, accounts } } = useEth();
-  const [voterAddr, setVoterAddr] = useState("");
+  
   
   const handleInputChange = (e) => {
     setVoterAddr(e.target.value);
   };
 
   async function addVoter() {
-      try {
-        await contract.methods.addVoter(voterAddr).send({from: accounts[0]});
-        alert("Voter add Successfully");
-        setVoterAddr("");
-      } catch(error) {
-        alert("Error in adding Voter");
-      }
+    try {
+      await contract.methods.addVoter(voterAddr).send({from: accounts[0]});
+      alert("Voter added Successfully");
+      setVoterAddr('');
+    } catch(error) {
+      alert("Error");
+    }     
   };
   
   return (
+    
       <div>
-        {currentStatus === "0" && (
-          <div>
-            <input
+        <Button colorScheme='teal' size='md' onClick={addVoter} disabled={!isOwner} >
+        Add Voter
+        </Button>
+          <input
               type="text"
-              placeholder="proposal"
+              placeholder="Voter Address"
               value={voterAddr}
               onChange={handleInputChange}
             />
-            <Button colorScheme='teal' size='md' onClick={addVoter}>
-              AddVoter:
-            </Button>
-          </div>
-            
-        )}
       </div>
   );
 }
